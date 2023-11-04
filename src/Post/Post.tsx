7638a06a4e4da80be073b3119ccba9f59c8c0415
Alexander Shchirov <shchirov1998@mail.ru>
2023-11-04 17:29:09 +0300

    commit-3

--- src/Post/Post.tsx
+++ src/Post/Post.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export const Test = () => {
     return(
         <div>
-            <h2>Post2</h2>
+            <h2>Post3</h2>
         </div>
     )
 }
--- src/User/User.tsx
+++ src/User/User.tsx
@@ -4,7 +4,7 @@ import React from "react";
 export const Test = () => {
     return(
         <div>
-            <h2>User2</h2>
+            <h2>User3</h2>
         </div>
     )
 }
